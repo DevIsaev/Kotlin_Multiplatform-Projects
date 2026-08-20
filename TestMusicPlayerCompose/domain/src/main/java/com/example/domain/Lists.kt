4c5f8bdad04cb08638464b775b65b90ede09b779
unknown <124510757+DevIsaev@users.noreply.github.com>
2026-08-20 21:49:10 +0700

    ComposeМузПлеер
    Создан в доменном слое usecase ToggleAudioPlayback - переключение воспроизведения аудио

---
+++ TestMusicPlayerCompose/domain/src/main/java/com/example/domain/Lists.kt
@@ -0,0 +1,6 @@
+package com.example.domain
+
+enum class Lists (var value: String){
+    ON_DEVICE("onDevice"),
+    FAVOURITES("favourites")
+}
---
+++ TestMusicPlayerCompose/domain/src/main/java/com/example/domain/usecases/ToggleAudioPlayback.kt
@@ -0,0 +1,57 @@
+package com.example.domain.usecases
+
+import com.example.domain.AudioPlayer
+import com.example.domain.Lists
+import com.example.domain.logger.Logger
+import com.example.domain.models.AudioDomain
+import com.example.domain.utils.IsFileAlailableByUri
+
+class ToggleAudioPlayback(private var audioPlayer: AudioPlayer, private var isFileAlailableByUri: IsFileAlailableByUri, private var logger: Logger) {
+    operator fun invoke(
+        onDeviceLists: List<AudioDomain>,
+        favouriteList: List<AudioDomain>,
+        selectedAudio: AudioDomain,
+        currentListType: Lists,
+        onError:()->Unit,
+        isOnTrackEnd: Boolean,
+        stopCurrentTrack: Boolean,
+        currentTrackNonInPlaylist:Boolean):Triple <List<AudioDomain>,List<AudioDomain>, AudioDomain?> {
+        var currentTrack: AudioDomain? = null
+        var updatedOnDeviceList = onDeviceLists.map { audio ->
+            if (audio.id == selectedAudio.id) {
+                if(!isFileAlailableByUri(audio.uri)) { onError()
+                    return@map audio.copy(isPlaying = true, isOnPause = false)}
+                if(stopCurrentTrack) { return@map audio.copy(isPlaying = false, isOnPause = false) }
+
+                var willPlay=if(isOnTrackEnd&&(if(currentListType==Lists.ON_DEVICE) onDeviceLists.size else favouriteList.size)==1&&!currentTrackNonInPlaylist)
+                    audio.isPlaying else !audio.isPlaying
+                var willBeOnPause=audio.isPlaying&&!isOnTrackEnd
+                var wasOnPause=audio.isOnPause
+
+                if(willPlay&&wasOnPause) {
+                    audioPlayer.play(audio)
+                }
+                    else if (willPlay) {
+                        audioPlayer.prepare(audio)
+                        audioPlayer.play(audio)
+                    } else if (willBeOnPause) {
+                        audioPlayer.pause(audio)
+                    }
+                    var track = audio.copy(isPlaying = willPlay, isOnPause = willBeOnPause)
+                    currentTrack = track
+                    track
+                }
+                else
+                    audio.copy(isPlaying = false, isOnPause = false)
+        }
+
+        var updatedFalouritesList=favouriteList.map { audio->
+            if(audio.id==currentTrack?.id)
+                currentTrack!!.copy()
+            else
+                audio.copy(isPlaying = false, isOnPause = false)
+
+        }
+        return Triple(updatedOnDeviceList,updatedFalouritesList,currentTrack)
+    }
+}
